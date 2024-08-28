ccc75c08a21fe4fc057d4cef682e4694668a1eca
Daniel Fros <daniel.fros@grupomaccio.com>
2024-08-28 14:43:07 -0300

    more files: functions

---
+++ functions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cubo_Pow2(int numero){
+    int resultado = numero * numero;
+    return resultado;
+}
+
+int main (){
+    printf("%d",cubo_Pow2(4));
+    return 0;
+
+}
+
