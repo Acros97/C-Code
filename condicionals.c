fac8ff571cae4aaa18521d235c93abf76edafaa7
Daniel Fros <daniel.fros@grupomaccio.com>
2024-08-29 10:13:32 -0300

    more example files

---
+++ condicionals.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int numero1 = 5;
+    int numero2 = 10;
+
+    if(numero1 > numero2)
+        printf("Numero 1 es mayor a numero 2\n");
+    else
+        printf("Numero 2 es mayor a numero 1\n");
+
+    return 0;
+}
