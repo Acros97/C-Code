710581c19901e799726e8685e5a855df1729c9bc
Daniel Fros <daniel.fros@grupomaccio.com>
2024-09-02 10:20:43 -0300

    change

--- memoryDirection.c
+++ memoryDirection.c
@@ -3,9 +3,16 @@
 
 int main(){
 int entero  = 5;
-printf("%p\n", &entero); /*Accede mediante el puntero "%p" la 
+printf("Entero direccion memoria: %p\n", &entero); /*Accede mediante el puntero "%p" la 
                          dirección de memoria donde se guarda el valor 5. En este caso me guardó
-                         en la dirección 0061FF1C*/ 
+                         en la dirección 0061FF1C (puede variar) */ 
+
+// Declaracion de punteros a partir de una variable (en este caso double)
+double numeroFlotante = 2.1;
+double *Puntero_numeroFlotante = &numeroFlotante;
+ printf("Numero flotante direccion memoria: %p\n",Puntero_numeroFlotante); 
+
+
 
 
 }
