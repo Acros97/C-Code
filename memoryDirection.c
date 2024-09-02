dc094cba5a239e48c375dbd456d7f78fc25b3d65
Daniel Fros <daniel.fros@grupomaccio.com>
2024-09-02 10:09:09 -0300

    moreExamples

---
+++ memoryDirection.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(){
+int entero  = 5;
+printf("%p\n", &entero); /*Accede mediante el puntero "%p" la 
+                         dirección de memoria donde se guarda el valor 5. En este caso me guardó
+                         en la dirección 0061FF1C*/ 
+
+
+}
