4b4a0a7f2ec697653da7078a0de25c0aefb26763
Daniel Fros <50487427+Acros97@users.noreply.github.com>
2024-09-09 15:30:30 -0300

    Agregando suceción fibonacci, usando función recursiva

---
+++ fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int fibonacci(int number){
+	if(number > 2)
+		return fibonacci(number - 1) + fibonacci(number - 2);
+	else if(number == 2)
+		return 1;
+	else if (number == 1)
+		return 1;
+	else if (number == 0)
+		return 0;
+}
+
+
+int main(void){
+	int number;
+	// Fibonacci es una sucesion infinita, se imprimen hasta los primeros 20 digitos
+	for(number = 0; number <=20; number++ ){
+		printf("%d\n", fibonacci(number));
+	}
+
+
+	return 0;
+}
