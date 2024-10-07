bdf8591f6048d103db0e974d59570da4c36b5ca3
Daniel Fros <daniel.fros@grupomaccio.com>
2024-10-07 16:54:36 -0300

    more study case *memory manage

---
+++ PRACTICES/Practice_memory_manage.c
@@ -0,0 +1,41 @@
+// Oct. 2024. Daniel Fros
+
+#include <stdio.h>
+// defining a struct for data and other stuff
+typedef struct {
+    int *data;
+    size_t size;
+    size_t capacity;    
+} IntList;
+
+void init_list(IntList *list){
+    list->capacity = 3; // default 3 (optional)
+    list->size = 5; // default 5 (optional)
+    list->data = malloc(list->capacity * sizeof(int)); // dinamic data assigment for 3 numbers
+   
+   // list->data = NULL;  // -> FORCING DATA == NULL
+    if(list -> data == NULL){
+        printf("Assigment error\n");
+        exit(1);
+    }
+}
+
+
+
+int main(){
+
+    printf("===TESTING===\n");
+    IntList listData; // not a pointer
+    init_list(&listData); // listData as a pointer
+
+    printf("capacity: %zu\n", listData.capacity);
+    printf("size: %zu\n", listData.size);
+    printf("data: %p\n", (void*)listData.data);  // Show memory direction of 'data'
+
+
+    // free the memory
+    free(listData.data);
+    
+
+    return 0;
+}
