43e151507f140ace95dfe7b4f9984d044b1a9471
Daniel Fros <daniel.fros@grupomaccio.com>
2024-11-04 15:04:43 -0300

    openFileDialog

--- PRACTICES/rle_compress_example.c
+++ PRACTICES/rle_compress_example.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <Windows.h>
+#include <commdlg.h>
 	
+
+
+	void openFileDialog(char* filepath,DWORD filePathSize){
+		OPENFILENAME ofname;
+		ZeroMemory(&ofname,sizeof(ofname));
+
+		ofname.lStructSize = sizeof(ofname);
+		ofname.hwndOwner = NULL;
+		ofname.lpstrFile = filepath;	
+		ofname.lpstrFile[0] = '\0';
+		ofname.nMaxFile = filePathSize;
+		ofname.lpstrFilter = "Archivos de texto\0*.txt";
+		ofname.nFilterIndex = 1;
+		ofname.lpstrFileTitle = NULL;
+		ofname.nMaxFileTitle = 0;
+		ofname.lpstrInitialDir = NULL;
+		ofname.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+
+		if(GetOpenFileName(&ofname))
+			printf("Se seleccionó el archivo: %s", filepath);
+		else
+			printf("No se seleccionó ningún archivo.\n");
+
+		
+
+	}
+
+
+	/// @brief función principal que comprime los datos usando el metodo Run-Length Encoding (RLE)
 	void compressRLE(const char* inputFile, const char* outputFile){
 		FILE *input = fopen(inputFile,"rb");
 		FILE *output = fopen(outputFile,"wb");
@@ -40,9 +72,10 @@
 	}
 
 	int main(){
-		const char* inputFile = "prueba.txt"; // modificar esto
+		char filePath[MAX_PATH];
+		openFileDialog(filePath,MAX_PATH);
 		const char* outputFile = "output.rle";	// archivo de salida en formato .rle 
-		compressRLE(inputFile, outputFile);
+		compressRLE(filePath, outputFile);
 
 		return 0;
 	}
